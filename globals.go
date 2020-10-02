b3ecb7ff6899fdb1fe6c462e9b7c14bc9d6007ce
Bruce <bruce@r-p-r.co.uk>
2020-10-02 16:15:31 +0100

    OWS5 interface changes to work around non-exported global constants

--- globals.go
+++ globals.go
@@ -7,41 +7,34 @@ import (
 // https://cdn-shop.adafruit.com/datasheets/ads1115.pdf
 
 type (
-	// ConfigInputMultiplexer config input multiplexer
-	ConfigInputMultiplexer uint16
 	// ConfigGain config gain amplifier
 	ConfigGain uint16
 	// ConfigDataRate config data rate
 	ConfigDataRate uint16
 )
 
+
 const (
 	// Bit [15] OS: Operational status/single-shot conversion start
-	configOperationNone   uint16 = 0x0000 // 0 : No effect
-	configOperationSingle uint16 = 0x8000 // 1 : Begin a single conversion (when in power-down mode)
+	configOperationMask uint16 = 0x8000
+	ConfigOperationNone   uint16 = 0x0000 // 0 : No effect
+	ConfigOperationSingle uint16 = 0x8000 // 1 : Begin a single conversion (when in power-down mode)
 
 	// Bits [14:12] MUX[2:0]: Input multiplexer configuration (ADS1115 only)
-
-	// ConfigInputMultiplexerDifferential01 000 : AINP = AIN0 and AINN = AIN1 (default)
-	ConfigInputMultiplexerDifferential01 ConfigInputMultiplexer = 0x0000
-	// ConfigInputMultiplexerDifferential03 001 : AINP = AIN0 and AINN = AIN3
-	ConfigInputMultiplexerDifferential03 ConfigInputMultiplexer = 0x1000
-	// ConfigInputMultiplexerDifferential13 010 : AINP = AIN1 and AINN = AIN3
-	ConfigInputMultiplexerDifferential13 ConfigInputMultiplexer = 0x2000
-	// ConfigInputMultiplexerDifferential23 011 : AINP = AIN2 and AINN = AIN3
-	ConfigInputMultiplexerDifferential23 ConfigInputMultiplexer = 0x3000
-	// ConfigInputMultiplexerSingle0 100 : AINP = AIN0 and AINN = GND
-	ConfigInputMultiplexerSingle0 ConfigInputMultiplexer = 0x4000
-	// ConfigInputMultiplexerSingle1 101 : AINP = AIN1 and AINN = GND
-	ConfigInputMultiplexerSingle1 ConfigInputMultiplexer = 0x5000
-	// ConfigInputMultiplexerSingle2 110 : AINP = AIN2 and AINN = GND
-	ConfigInputMultiplexerSingle2 ConfigInputMultiplexer = 0x6000
-	// ConfigInputMultiplexerSingle3 111 : AINP = AIN3 and AINN = GND
-	ConfigInputMultiplexerSingle3 ConfigInputMultiplexer = 0x7000
+	configInputMultiplexerMask uint16 = 0x7000
+	ConfigInputMultiplexerDifferential01 uint16 = 0x0000 // 000 : AINP = AIN0 and AINN = AIN1 (default)
+	ConfigInputMultiplexerDifferential03 uint16 = 0x1000 // 001 : AINP = AIN0 and AINN = AIN3
+	ConfigInputMultiplexerDifferential13 uint16 = 0x2000 // 010 : AINP = AIN1 and AINN = AIN3
+	ConfigInputMultiplexerDifferential23 uint16 = 0x3000 // 011 : AINP = AIN2 and AINN = AIN3
+	ConfigInputMultiplexerSingle0        uint16 = 0x4000 // 100 : AINP = AIN0 and AINN = GND
+	ConfigInputMultiplexerSingle1        uint16 = 0x5000 // 101 : AINP = AIN1 and AINN = GND
+	ConfigInputMultiplexerSingle2        uint16 = 0x6000 // 110 : AINP = AIN2 and AINN = GND
+	ConfigInputMultiplexerSingle3        uint16 = 0x7000 // 111 : AINP = AIN3 and AINN = GND
 
 	// Bits [11:9] PGA[2:0]: Programmable gain amplifier configuration (ADS1114 and ADS1115 only)
 
 	// ConfigGain2_3 gain amplifier 2/3 is +/-6.144V range (default)
+	configGainMask uint16 = 0x0E00
 	ConfigGain2_3 ConfigGain = 0x0000 // 000
 	// ConfigGain1 gain amplifier 1 is +/-4.096V range
 	ConfigGain1 ConfigGain = 0x0200 // 001
@@ -55,11 +48,12 @@ const (
 	ConfigGain16 ConfigGain = 0x0A00 // 101
 
 	// Bit [8] MODE: Device operating mode
-	configOperatingModeContinuous uint16 = 0x0000 // 0 : Continuous conversion mode
-	configOperatingModeSingle     uint16 = 0x0100 // 1 : Power-down single-shot mode (default)
+	configOperatingModeMask uint16 = 0x0100
+	ConfigOperatingModeContinuous uint16 = 0x0000 // 0 : Continuous conversion mode
+	ConfigOperatingModeSingle     uint16 = 0x0100 // 1 : Power-down single-shot mode (default)
 
 	// Bits [7:5] DR[2:0]: Data rate
-
+	configDataRateMask ConfigDataRate = 0x00E0
 	// ConfigDataRate8 data rate of 8 samples per second
 	ConfigDataRate8 ConfigDataRate = 0x0000 // 000
 	// ConfigDataRate16 data rate of 16 samples per second
@@ -78,42 +72,46 @@ const (
 	ConfigDataRate860 ConfigDataRate = 0x00E0 // 111
 
 	// Bit [4] COMP_MODE: Comparator mode (ADS1114 and ADS1115 only)
-	configComparatorModeTraditional uint16 = 0x0000 // 0 : Traditional comparator with hysteresis (default)
-	configComparatorModeWindow      uint16 = 0x0010 // 1 : Window comparator
+	configComparatorModeMask uint16 = 0x0010
+	ConfigComparatorModeTraditional uint16 = 0x0000 // 0 : Traditional comparator with hysteresis (default)
+	ConfigComparatorModeWindow      uint16 = 0x0010 // 1 : Window comparator
 
 	// Bit [3] COMP_POL: Comparator polarity (ADS1114 and ADS1115 only)
-	configComparatorPolarityLow  uint16 = 0x0000 // 0 : Active low (default)
-	configComparatorPolarityHigh uint16 = 0x0008 // 1 : Active high
+	configComparatorPolarityMask uint16 = 0x0008
+	ConfigComparatorPolarityLow  uint16 = 0x0000 // 0 : Active low (default)
+	ConfigComparatorPolarityHigh uint16 = 0x0008 // 1 : Active high
 
 	// Bit [2] COMP_LAT: Latching comparator (ADS1114 and ADS1115 only)
-	configLatchingComparatorOff uint16 = 0x0000 // 0 : Non-latching comparator (default)
-	configLatchingComparatorOn  uint16 = 0x0004 // 1 : Latching comparator
+	configLatchingComparatorMask uint16 = 0x0004
+	ConfigLatchingComparatorOff uint16 = 0x0000 // 0 : Non-latching comparator (default)
+	ConfigLatchingComparatorOn  uint16 = 0x0004 // 1 : Latching comparator
 
 	// Bits [1:0] COMP_QUE: Comparator queue and disable (ADS1114 and ADS1115 only)
-	configComparatorQueue1   uint16 = 0x0000 // 00 : Assert after one conversion
-	configComparatorQueue2   uint16 = 0x0001 // 01 : Assert after two conversions
-	configComparatorQueue4   uint16 = 0x0002 // 10 : Assert after four conversions
-	configComparatorQueueOff uint16 = 0x0003 // 11 : Disable comparator (default)
+	configComparatorMask uint16 = 0x0003
+	ConfigComparatorQueue1   uint16 = 0x0000 // 00 : Assert after one conversion
+	ConfigComparatorQueue2   uint16 = 0x0001 // 01 : Assert after two conversions
+	ConfigComparatorQueue4   uint16 = 0x0002 // 10 : Assert after four conversions
+	ConfigComparatorQueueOff uint16 = 0x0003 // 11 : Disable comparator (default)
 
 	// register pointer
 	registerPointerConversion byte = 0x0000
 	registerPointerConfig     byte = 0x0001
 
 	// configDefault is the default config
-	configDefault = configOperationSingle | // missing ConfigInputMultiplexer and ConfigGain
-		configOperatingModeSingle | // missing ConfigDataRate
-		configComparatorModeTraditional | configComparatorPolarityLow | configLatchingComparatorOff | configComparatorQueueOff
+	configDefault = ConfigOperationSingle | ConfigInputMultiplexerSingle0 | // missing ConfigGain
+		ConfigOperatingModeSingle | // missing ConfigDataRate
+		ConfigComparatorModeTraditional | ConfigComparatorPolarityLow | ConfigLatchingComparatorOff | ConfigComparatorQueueOff
 )
 
 // ADS struct to interface with the Analog-to-Digital Converter.
 // Call NewADS to create a new one.
 type ADS struct {
-	busCloser              *i2c.BusCloser
-	dev                    *i2c.Dev
-	configInputMultiplexer uint16
-	configGain             uint16
-	configDataRate         uint16
-	config                 []byte
-	write                  []byte
-	read                   []byte
+	busCloser      *i2c.BusCloser
+	dev            *i2c.Dev
+	configGain     uint16
+	configDataRate uint16
+	config         []byte
+	write          []byte
+	read           []byte
 }
+
